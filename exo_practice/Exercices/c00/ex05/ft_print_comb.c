70def720d264bf0dc454db52971f1ceec3178bde
William <wnetomarques@gmail.com>
2025-04-30 13:01:18 +0200

    changement

--- exo_practice/Exercices/c00/ex05/ft_print_comb.c
+++ exo_practice/Exercices/c00/ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: codephenix2 <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 12:52:10 by codephenix2       #+#    #+#             */
-/*   Updated: 2025/04/29 12:53:50 by codephenix2      ###   ########.fr       */
+/*   Updated: 2025/04/30 12:59:14 by codephenix2      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,40 @@ void	displayer(char c, char d, char u)
 	ft_putchar(c);
 	ft_putchar(d);
 	ft_putchar(u);
-	ft_putchar(',');
-	ft_putchar(' ');
 }
+
 void	ft_print_comb(void)
 {
-	char c;
-	char d;
-	char u;
+	char	c;
+	char	d;
+	char	u;
 
-	c = 0;
-	while (c == 7 && d == 8 && u == 9)
+	c = '0';
+	while (c <= '7')
 	{
 		d = c + 1;
-		u = d + 1;
-		if (u < 9)
-			u++
-		c++
+		while (d <= '8')
+		{
+			u = d + 1;
+			while (u <= '9')
+			{
+				displayer(c, d, u);
+				if (c != '7')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				u++;
+			}
+			d++;
+		}
+		c++;
 	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	ft_putchar('\n');
+	return (0);
 }
